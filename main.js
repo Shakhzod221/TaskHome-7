9f05b9bdefa39106c0a3607a697a34be253e9259
Shakhzod221 <130886474+Shakhzod221@users.noreply.github.com>
2023-10-01 00:56:07 +0500

    Task Home

---
+++ main.js
@@ -0,0 +1,35 @@
+class keyboard {
+    constructor(name, TheTask){
+        this.name = name;
+        this.TheTask = TheTask;
+    }
+    getName(){
+        console.log(this.name);
+    }
+    getTheTask(){
+        console.log(this.TheTask);
+    }
+}
+
+let ctrlA = new keyboard('Ctrl + A', 'Barchasini tanlang');
+let ctrlB = new keyboard('Ctrl + B', 'Qalin');
+let ctrlC = new keyboard('Ctrl + C', 'Nusxalash');
+let ctrlD = new keyboard('Ctrl + D', 'Pastga toldirish');
+let ctrlE = new keyboard('Ctrl + E', 'Markazni tekislash');
+let ctrlF = new keyboard('Ctrl + F', 'Toping');
+let ctrlG = new keyboard('Ctrl + G', 'Goto');
+let ctrlH = new keyboard('Ctrl + H', 'Ozgartirish');
+let ctrlI = new keyboard('Ctrl + I', 'Kursiv');
+let ctrlJ = new keyboard('Ctrl + J', 'Asoslash');
+let ctrlK = new keyboard('Ctrl + K', 'Havolalarni kiritish');
+let ctrlL = new keyboard('Ctrl + L', 'Tekislash')
+let ctrlN = new keyboard('Ctrl + N', 'Yangi ishchi daftar');
+let ctrlO = new keyboard('Ctrl + O', 'Ochish');
+let ctrlP = new keyboard('Ctrl + P', 'Chop etish');
+let ctrlR = new keyboard('Ctrl + R', 'Ongga tekislash');
+let ctrlS = new keyboard('Ctrl + S', 'Saqlash');
+let ctrlU = new keyboard('Ctrl + U', 'Tagiga chizish');
+let ctrlV = new keyboard('Ctrl + V', 'Joylashtirish');
+let ctrlW = new keyboard('Ctrl + W', 'Yopish');
+let ctrlX = new keyboard('Ctrl + X', 'Kesish');
+let ctrlZ = new keyboard('Ctrl + z', 'Takrorlash');
